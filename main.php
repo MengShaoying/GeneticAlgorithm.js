532533d3708d19f66667c57e73e018e3280b2c29
MengShaoying <mengshaoying@aliyun.com>
2019-02-17 18:59:14 +0800

    增加文件保存

--- main.php
+++ main.php
@@ -3,6 +3,16 @@ require 'Gene.php';
 require 'Chromosome.php';
 require 'Population.php';
 
+function addCsv($file, $maximum, $average, $minimum, $variance)
+{
+    static $id = 0;
+    $id++;
+    if (!is_file($file)) {
+        file_put_contents($file, 'id,maximum,average,minimum,variance' . PHP_EOL);
+    }
+    file_put_contents($file, "$id,$maximum,$average,$minimum,$variance" . PHP_EOL, FILE_APPEND);
+}
+
 $pop = new Population(100, 4, ['G', 'T', 'A', 'C']);
 for ($i = 0; $i < 20; $i++) {
     $maximum = $pop->getMaximumFitnessChromosome()->fitness();
@@ -17,5 +27,6 @@ for ($i = 0; $i < 20; $i++) {
     echo '  ';
     echo 'variance ' . sprintf('%.2f', $variance);
     echo PHP_EOL;
+    addCsv('log.csv', $maximum, $average, $minimum, $variance);
     $pop = $pop->getNextPopulation(10, 0.01);
 }
